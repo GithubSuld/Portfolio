22ea5d443a04449c6d2d0f1b5bcf0be7f72828b7
GithubSuld <exclusive.suld@gmail.com>
2024-07-23 12:38:39 -0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -5,9 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
         var currentColor = document.body.style.backgroundColor;
         if (currentColor === "white") {
             document.body.style.backgroundColor = "#0C151D";
+            document.body.classList.remove("light-mode");
         } else {
             document.body.style.backgroundColor = "white";
+            document.body.classList.add("light-mode");
         }
     });
 });
-
